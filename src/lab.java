3959df764e7ba12d9a67429e2160e21350aebfc3
annaleoprok <Piefish78@gmail.com>
2021-06-21 23:39:09 +0900

    Initial commit

---
+++ src/lab.java
@@ -0,0 +1,9 @@
+public class lab {
+    //x^3+ 2,5х^2– х – 2,5 = 0, корни: -2,5; -1; 1
+    public static void main(String[] args) {
+        double a = -2.5;
+        double b = 1;
+        double x = -1;
+        System.out.println( a*a*a+2.5*b*b-x-2.5);
+    }
+}
